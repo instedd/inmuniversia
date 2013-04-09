1773172db7e61d321a8735718ae6ceb4f4dbb61d
spalladino <spalladino@localhost>
2013-04-09 18:27:21 -0300

    Added superuser role to default user

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,4 @@ Refinery::Vaccines::Engine.load_seed
 # Generate initial admin user as initial Refinery setup generates redirect loop
 admin = User.create email: 'admin@example.com', password: 'ChangeMe'
 admin.add_role :refinery
+admin.add_role :superuser
