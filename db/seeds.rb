36e6be6a1ec555c9871cdab9e0725bfb20e4dae3
spalladino <spalladino@localhost>
2013-04-09 18:16:32 -0300

    Create admin user in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Emanuel', :city => cities.first)
-
 # Added by Refinery CMS Pages extension
 Refinery::Pages::Engine.load_seed
 
 # Added by Refinery CMS Vaccines extension
 Refinery::Vaccines::Engine.load_seed
+
+# Generate initial admin user as initial Refinery setup generates redirect loop
+admin = User.create email: 'admin@example.com', password: 'ChangeMe'
+admin.add_role :refinery
