d87d42c6eede62161d34bf425a9d19bfd7000bc4
spalladino <spalladino@localhost>
2013-04-15 16:43:00 -0300

    Initialize refinery user if seeds unsuccessful in tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,10 @@ RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
   config.include Devise::TestHelpers, type: :controller
 
+  config.before(:all) do
+    User.new(email: 'admin@example.com', password: 'ChangeMe').create_first if User.count == 0
+  end
+
   # Uncomment to view full backtraces
   # config.backtrace_clean_patterns = []
 end
