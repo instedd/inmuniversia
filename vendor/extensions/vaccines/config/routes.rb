f1df4a067d9d2876fc00a7a93c08ba8667d35f52
spalladino <spalladino@localhost>
2013-04-15 12:02:41 -0300

    Fixed diseases and vaccines routes

--- vendor/extensions/vaccines/config/routes.rb
+++ vendor/extensions/vaccines/config/routes.rb
@@ -2,6 +2,22 @@ Refinery::Core::Engine.routes.append do
 
   # Frontend routes
   namespace :vaccines do
+    resources :diseases, :only => [:index, :show]
+  end
+
+  # Admin routes
+  namespace :vaccines, :path => '' do
+    namespace :admin, :path => 'refinery/vaccines' do
+      resources :diseases, :except => :show do
+        collection do
+          post :update_positions
+        end
+      end
+    end
+  end
+
+  # Frontend routes
+  namespace :vaccines do
     resources :vaccines, :path => '', :only => [:index, :show]
   end
 
@@ -9,40 +25,6 @@ Refinery::Core::Engine.routes.append do
   namespace :vaccines, :path => '' do
     namespace :admin, :path => 'refinery' do
       resources :vaccines, :except => :show do
-        collection do
-          post :update_positions
-        end
-      end
-    end
-  end
-
-
-  # Frontend routes
-  namespace :vaccines do
-    resources :diseases, :only => [:index, :show]
-  end
-
-  # Admin routes
-  namespace :vaccines, :path => '' do
-    namespace :admin, :path => 'refinery/vaccines' do
-      resources :diseases, :except => :show do
-        collection do
-          post :update_positions
-        end
-      end
-    end
-  end
-
-
-  # Frontend routes
-  namespace :vaccines do
-    resources :diseases, :only => [:index, :show]
-  end
-
-  # Admin routes
-  namespace :vaccines, :path => '' do
-    namespace :admin, :path => 'refinery/vaccines' do
-      resources :diseases, :except => :show do
         collection do
           post :update_positions
         end
