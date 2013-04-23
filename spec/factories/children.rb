c71ed17f1f9f520aec7647473a5e0bb32285381e
spalladino <spalladino@localhost>
2013-04-23 20:04:07 -0300

    Fix in children factory

--- spec/factories/children.rb
+++ spec/factories/children.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :child do
     sequence(:name) {|n| "Joe #{n}"}
     date_of_birth "2013-04-10 16:52:57"
-    gender "Male"
+    gender "male"
     parent
 
     trait :with_vaccinations do
