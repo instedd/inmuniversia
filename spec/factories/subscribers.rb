a25a382736ee8ae4dad8983378afd383c11291b2
spalladino <spalladino@localhost>
2013-04-24 17:49:44 -0300

    Fixed tests due to new subscriber validations

--- spec/factories/subscribers.rb
+++ spec/factories/subscribers.rb
@@ -1,6 +1,9 @@
 FactoryGirl.define do
   factory :subscriber, aliases: [:parent] do
     sequence(:email) { |n| "subscriber.#{n}@example.com"}
+    first_name "John"
+    last_name "Doe"
+    zip_code "12345"
     password "Password1!"
   end
 end
