d7d5dd810bdb8ac1d7402ec65fad619a78cde508
spalladino <spalladino@localhost>
2013-04-26 16:08:01 -0300

    Nav link helper for header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def nav_link(text, path, *controllers)
+    klazz = controllers.include?(params[:controller]) ? "active" : ""
+    content_tag :li, :class => klazz do
+      link_to text, path
+    end
+  end
+
 end
