515fa1c9df7191cbb6f61183a7a60b99f4aa9e18
Juan Blandini <jblandini@manas.com.ar>
2013-04-26 15:10:54 -0300

    Commit parcial para 2680 [SignUp] Pedir datos de mobile durante signup

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,16 @@ Inmuniversia::Application.routes.draw do
     registrations: 'subscribers/registrations'
   }
 
+  devise_scope :subscriber do
+    get '/subscribers/mobile_configuration' => 'subscribers/registrations#mobile_configuration'
+    post '/subscribers/add_mobile_number_and_send_verification_code' => 'subscribers/registrations#add_mobile_number_and_send_verification_code'
+    post '/subscribers/submit_verification_code' => 'subscribers/registrations#submit_verification_code'
+  end
+
   # Hide vaccine and disease resources from refinery and handle them manually
   resources :diseases, only: [:show, :index], path: '/vaccines/diseases'
   resources :vaccines, only: [:show, :index]
-  
+
   resources :children, only: :create
 
   resources :channel_emails, only: [:create, :update, :destroy]
