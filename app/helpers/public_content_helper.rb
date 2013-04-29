3905c764754f948f898481088efe7cb9a2c92365
spalladino <spalladino@localhost>
2013-04-29 14:03:01 -0300

    Fix for helper method on nil sections

--- app/helpers/public_content_helper.rb
+++ app/helpers/public_content_helper.rb
@@ -2,7 +2,7 @@ module PublicContentHelper
 
   def section(label, name, object)
     (content_tag :section, id: name, name: name do
-      content_tag(:h1, label) + object.send(name).html_safe
+      content_tag(:h1, label) + object.send(name).try(:html_safe)
     end) + content_tag(:hr)
   end
 
