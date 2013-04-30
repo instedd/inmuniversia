c8cf976372d8159f8f2fc333955b760d9653e308
spalladino <spalladino@localhost>
2013-04-30 15:44:50 -0300

    Save last tab opened in dashboard

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,4 +20,27 @@ module ApplicationHelper
     end
   end
 
+  def tab_pane(opts={})
+    active = opts.delete(:active)
+    opts[:class] ||= "" 
+    opts[:class] << ' active in' if active
+    opts[:class] << ' tab-pane'
+    
+    content_tag :div, opts do
+      yield
+    end
+  end
+
+  def tab_button(text, target, opts={})
+    active = opts.delete(:active)
+    opts[:href] = "##{target}"
+    opts[:'data-toggle'] = "tab"
+    opts[:'data-tab-name'] = target
+    opts[:class] ||= "" 
+    opts[:class] << ' active' if active
+    opts[:class] << ' btn'
+    
+    content_tag :button, text, opts
+  end
+
 end
