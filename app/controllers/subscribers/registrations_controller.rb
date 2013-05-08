c8d00dea20f0d07964cb19c87e1ec12cb5fe58b2
Juan Blandini <jblandini@manas.com.ar>
2013-05-08 11:58:53 -0300

    Tests for 2680. [SignUp] Pedir datos de mobile durante signup

--- app/controllers/subscribers/registrations_controller.rb
+++ app/controllers/subscribers/registrations_controller.rb
@@ -1,12 +1,13 @@
 class Subscribers::RegistrationsController < Devise::RegistrationsController
+
+  # prepend_before_filter :authenticate_subscriber!, force: true
+
   def after_sign_up_path_for(resource)
     subscribers_mobile_configuration_path
   end
-  # def after_sign_in_path_for(resource)
-  #   dashboard_path
-  # end
 
   def mobile_configuration
+    # authenticate_subscriber!
     @email = current_subscriber.email
     @channel = Channel::Sms.new
   end
@@ -20,32 +21,31 @@ class Subscribers::RegistrationsController < Devise::RegistrationsController
       @error = true
     else
       if params[:channel_sms_resend]
-        @channel = Channel.find(params[:channel_sms_resend])
+        @channel = Channel::Sms.find(params[:channel_sms_resend])
         @resend = true
       else
         @channel = current_subscriber.sms_channels.new(:address => params[:channel_sms][:address])
       end
     end
 
-    @channel.send_verification_code
-
     if @channel.save
+      @channel.send_verification_code
+      @channel.save
       # puts "Salvado correctamente"
-
     else
       flash[:notice] = @channel.errors.messages[:address][0]
       redirect_to subscribers_mobile_configuration_url()
     end
-
   end
 
   def submit_verification_code
     if params[:id] == ""
       redirect_to root_url
     else
-      @channel = Channel.find(params[:id])
-
+      @channel = Channel::Sms.find(params[:id])
       if (@channel.verification_code == params[:verification_code][0])
+        @channel.verification_code = nil
+        @channel.save!
         flash[:notice] = "El celular ha sido agregado correctamente"
         redirect_to dashboard_path
       else
