4d5ef133ff17d751d9d117ac1a672732891d080a
spalladino <spalladino@localhost>
2013-05-10 19:51:14 -0300

    Redirect user to home page on failed sign in

--- app/controllers/subscribers/sessions_controller.rb
+++ app/controllers/subscribers/sessions_controller.rb
@@ -1,2 +1,16 @@
 class Subscribers::SessionsController < Devise::SessionsController
+
+  def new
+    @login_resource = resource = build_resource
+    clean_up_passwords(resource)
+    render 'home/index'
+  end
+
+  def create
+    @login_resource = resource = warden.authenticate!(auth_options)
+    set_flash_message(:notice, :signed_in) if is_navigational_format?
+    sign_in(resource_name, resource)
+    respond_with resource, :location => after_sign_in_path_for(resource)
+  end
+
 end
