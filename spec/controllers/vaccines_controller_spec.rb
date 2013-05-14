532352a92136561366bf80d1dd970b93d7b02a54
Juan Blandini <jblandini@manas.com.ar>
2013-05-14 12:52:59 -0300

    2716. Adding tests

--- spec/controllers/vaccines_controller_spec.rb
+++ spec/controllers/vaccines_controller_spec.rb
@@ -25,10 +25,32 @@ describe VaccinesController do
   context "show" do
 
     let!(:vaccine) {create(:vaccine, published: true)}
+    let!(:subscriber) {create(:subscriber)}
+    let!(:comment_list) {create_list(:comment, 3, subscriber: subscriber, commentable: vaccine)}
 
     it "should render vaccine page" do
       get :show, id: vaccine.id
       response.should be_success
+    end
+
+    it "should return the comments in the right order" do
+      child1 = Comment.build_from(vaccine, subscriber, "test")
+      child2 = Comment.build_from(vaccine, subscriber, "test2")
+      child3 = Comment.build_from(vaccine, subscriber, "test3")
+
+      child1.save
+      child2.save
+      child3.save
+
+      child1.move_to_child_of(comment_list[1])
+      child2.move_to_child_of(comment_list[0])
+      child3.move_to_child_of(comment_list[1])
+
+      reference_hash = [{"father" => comment_list[0], "children" => [child2]}, {"father" => comment_list[1], "children" => [child1, child3]}, {"father" => comment_list[2], "children" => []}]
+
+      get :show, id: vaccine.id
+
+      assigns(:comments).should match_array(reference_hash)
     end
 
   end
--- spec/factories/vaccines.rb
+++ spec/factories/vaccines.rb
@@ -7,7 +7,7 @@ FactoryGirl.define do
     trait :with_doses do
       ignore { dose_count 3 }
       ignore { first_dose_at 1 }
-      
+
       after(:create) do |vaccine, evaluator|
         evaluator.dose_count.times do |i|
           create(:dose_by_age, age_value: (i + evaluator.first_dose_at), age_unit: 'year', vaccine: vaccine)
@@ -18,13 +18,13 @@ FactoryGirl.define do
     trait :with_monthly_doses do
       ignore { dose_count 3 }
       ignore { first_dose_at 1 }
-      
+
       after(:create) do |vaccine, evaluator|
         evaluator.dose_count.times do |i|
           create(:dose_by_age, age_value: (i + evaluator.first_dose_at), age_unit: 'month', vaccine: vaccine)
         end
       end
     end
-  
+
   end
 end
