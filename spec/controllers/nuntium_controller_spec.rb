80693cf484fe1f3b6d1f1042c1970e49e403bd78
Juan Blandini <jblandini@manas.com.ar>
2013-05-20 12:26:31 -0300

    2799. [SMS] Crear usuario vacio via SMS. Arreglando tests, y acomodando un poco

--- spec/controllers/nuntium_controller_spec.rb
+++ spec/controllers/nuntium_controller_spec.rb
@@ -2,39 +2,32 @@ require 'spec_helper'
 
 describe NuntiumController do
   sms_number = "12345678"
-  it "receives at and replies nothing" do
-    # Nuntium.any_instance.should_receive(:process).with('body' => 'foo')
-    #sacar esto a un modulo que inicializo en spec_helper, y le paso los parametros de user y pass
-    @request.env["HTTP_AUTHORIZATION"] = "Basic " + Base64::encode64("#{Settings.nuntium.incoming_username}:#{Settings.nuntium.incoming_password}")
-    post :receive_at, 'body' => 'foo'
 
-    response.should be_success
-    response.body.should be_blank
-  end
+  context "authenticated" do
+    before(:each) {hhtp_authorization @request, Settings.nuntium.incoming_username, Settings.nuntium.incoming_password}
 
-  it "receives at 'alta' msg and creates channel" do
-    @request.env["HTTP_AUTHORIZATION"] = "Basic " + Base64::encode64("#{Settings.nuntium.incoming_username}:#{Settings.nuntium.incoming_password}")
-    #expect to change 1 y juntar los dos tests en un each
-    Channel::Sms.count.should eq(0)
-    post :receive_at, :body => 'alta', :from => sms_number
-    Channel::Sms.count.should eq(1)
-    Channel::Sms.last.address.should eq(sms_number)
+    it "receives at and replies nothing" do
 
-    response.should be_success
-    response.body.should eq('Bienvenido a Inmuniversia')
-    response.content_type.should eq('text/plain')
-  end
+      post :receive_at, 'body' => 'foo'
 
-  it "receives at 'ALTA' msg and creates channel" do
-    @request.env["HTTP_AUTHORIZATION"] = "Basic " + Base64::encode64("#{Settings.nuntium.incoming_username}:#{Settings.nuntium.incoming_password}")
-    Channel::Sms.count.should eq(0)
-    post :receive_at, :body => 'ALTA', :from => sms_number
-    Channel::Sms.count.should eq(1)
-    Channel::Sms.last.address.should eq(sms_number)
+      response.should be_success
+      response.body.should be_blank
+    end
 
-    response.should be_success
-    response.body.should eq('Bienvenido a Inmuniversia')
-    response.content_type.should eq('text/plain')
+
+    ["alta", "ALTA", "alta2"].each do |message|
+      it "receives at #{message} msg and creates channel" do
+        expect {
+          post :receive_at, :body => message, :from => sms_number
+        }.to change(Channel::Sms, :count).by(1)
+
+        Channel::Sms.last.address.should eq(sms_number)
+
+        response.should be_success
+        response.body.should eq('Bienvenido a Inmuniversia')
+        response.content_type.should eq('text/plain')
+      end
+    end
   end
 
   it "performs basic http auth" do
